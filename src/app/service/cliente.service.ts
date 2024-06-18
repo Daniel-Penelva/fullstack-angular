cc91a7408de08fe4d36b9b6e3e75c73605a1e9c6
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-17 22:59:46 -0300

    Criando serviço Cliente e configurando HttpClient

---
+++ src/app/service/cliente.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  // URL da API
+  private url: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+}
+
+/* HttpClient - responsável por realizar as requisições */
