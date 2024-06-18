f28f1689b3387e6ce276e2ac01b26703d42b5bd3
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-18 01:18:08 -0300

    Listagem de clientes (Parte 1) - criando método selecionar()

--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../model/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,11 @@ export class ClienteService {
   private url: string = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
+
+  // Método para buscar todos os clientes
+  selecionar():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
 }
 
 /* HttpClient - responsável por realizar as requisições */
