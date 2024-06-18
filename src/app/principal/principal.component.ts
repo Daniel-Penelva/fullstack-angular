f8f4fe3b186bc431dffe05991c1efcc352d221fc
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-18 01:36:43 -0300

    Listagem de clientes (Parte 2) - criando o subscribe para buscar a lista de clientes

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/Cliente';
+import { ClienteService } from '../service/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -9,5 +11,18 @@ export class PrincipalComponent {
 
   // Variável para visibilidde dos botões
   btnCadastro: boolean = true;
+
+  // Json de lista de clientes
+  clientes: Cliente[] = [];
+
+  // Criando construtor para pode ter acesso ao ServiceCliente
+  constructor(private clienteService: ClienteService){
+
+  }
+
+  // Método para buscar todos os clientes
+  selecionar():void{
+    this.clienteService.selecionar().subscribe(retorno => this.clientes = retorno);
+  }
 
 }
